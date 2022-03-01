1fe8562fff9c42f64b136d1570a7e0deadd5a17d
Jhonatan Mideros <10jonmid@gmail.com>
2022-03-01 07:08:49 -0500

    [update] Ocultar Menu para pruebas en produccion

--- src/app/screens/AdminScreen.jsx
+++ src/app/screens/AdminScreen.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 // import Header from './../components/Header'
-import Menu from '../components/Menu'
+// import Menu from '../components/Menu'
 
 const AdminScreen = () => {
   return (
     <div className='grid grid-cols-[auto_1fr] w-full h-full'>
-      <Menu />
+      {/* <Menu /> */}
+      <h1>Menu</h1>
       <div className='flex flex-col'>
         {/* <Header /> */}
-        <h1>Prueba</h1>
+        <h1>Header</h1>
         <main className='grow bg-app-index'>
           <Outlet />
         </main>
