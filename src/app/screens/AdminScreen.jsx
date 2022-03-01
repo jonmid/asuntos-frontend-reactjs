3511a84759ac39968d4d09db568a479c5523138e
Jhonatan Mideros <10jonmid@gmail.com>
2022-03-01 07:03:41 -0500

    [update] Ocultar Header para pruebas en produccion

--- src/app/screens/AdminScreen.jsx
+++ src/app/screens/AdminScreen.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
-import Header from './../components/Header'
+// import Header from './../components/Header'
 import Menu from '../components/Menu'
 
 const AdminScreen = () => {
@@ -9,7 +9,8 @@ const AdminScreen = () => {
     <div className='grid grid-cols-[auto_1fr] w-full h-full'>
       <Menu />
       <div className='flex flex-col'>
-        <Header />
+        {/* <Header /> */}
+        <h1>Prueba</h1>
         <main className='grow bg-app-index'>
           <Outlet />
         </main>
